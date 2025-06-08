4498f75a2954d822bf64c8d2be49f6e56d85e8ea
Karlito16 <karlo.dimjasevic@gmail.com>
2025-06-08 19:59:00 +0200

    Fixing RFID GPIO pins that were in unstable state

--- ev-esp32-rc522-0625/main/ev-esp32-rc522-0625.c
+++ ev-esp32-rc522-0625/main/ev-esp32-rc522-0625.c
@@ -7,16 +7,29 @@
 
 static const char *TAG = "ev-esp32-rc522-0625";
 
+void bytes_to_hex_string(const byte *bytes, char *hex_str) {
+    sprintf(hex_str, "%02X%02X%02X%02X", bytes[0], bytes[1], bytes[2], bytes[3]);
+}
+
 void rfid_card_handler(const byte* cardId) {
-    printf("Card UID: ");
-    for (int i = 0; i < 4; i++) {
-        printf("%02X ", cardId[i]);
-    }
-    printf("\n");
+    char hex_str[4];
+    bytes_to_hex_string(cardId, hex_str);
+    ESP_LOGI(TAG, "Card ID: %s", hex_str);
+}
+
+void gpio_pins_setup(void) {
+    // TODO: Hardcoded pins for now, make this configurable
+    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_0, 1);
+
+    gpio_set_pull_mode(GPIO_NUM_19, GPIO_PULLUP_ONLY);  // MISO
 }
 
 void app_main(void)
 {
+    // Setup GPIO pins
+    gpio_pins_setup();
+
     // Setup Wi-Fi - blocking until connected to Wi-Fi
     init_wifi();
 
@@ -26,6 +39,8 @@ void app_main(void)
 
     // Setup MQTT client
     mqtt_app_start();
+
+    ESP_LOGI(TAG, "Setup completed");
 
     // Loop
     while (true) {
