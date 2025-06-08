e418b93248af3988d62770096a168c2ded0d7e93
Karlito <38817922+Karlito16@users.noreply.github.com>
2025-06-08 19:59:59 +0200

    Fixing RFID GPIO pins that were in unstable state (#4)

--- ev-esp32-rc522-0625/components/rfid-adapter/rfid-adapter.cpp
+++ ev-esp32-rc522-0625/components/rfid-adapter/rfid-adapter.cpp
@@ -2,8 +2,11 @@
 #include <SPI.h>
 #include "MFRC522.h"
 
+// TODO: Hardcoded pins for now, make this configurable
 #define SS_PIN 5
 #define RST_PIN 0
+
+static const char *TAG = "rfid-adapter";
 
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
@@ -11,17 +14,12 @@ extern "C" void init_rfid() {
     Serial.begin(115200);
     SPI.begin();
     rfid.PCD_Init();
-    Serial.println("RC522 Initialized. Bring a card near...");
+    ESP_LOGI(TAG, "RC522 Initialized");
 }
 
 extern "C" void check_for_new_card(void (*callback)(const byte* cardId)) {
     if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
-        Serial.print("Card UID: ");
-        for (byte i = 0; i < rfid.uid.size; i++) {
-            Serial.print(rfid.uid.uidByte[i], HEX);
-            Serial.print(" ");
-        }
-        Serial.println();
+        callback(rfid.uid.uidByte);
         rfid.PICC_HaltA();
     }
 }
