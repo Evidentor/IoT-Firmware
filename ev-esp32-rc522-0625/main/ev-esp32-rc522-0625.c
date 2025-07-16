3e543d716aa2175200d66ceb7a7f0b75b6c6ca0e
Karlito <38817922+Karlito16@users.noreply.github.com>
2025-07-16 12:48:42 +0200

    Feature/send telemetry after card scan (#6)
    * Sending the telemetry and receiving the ack.
    
    * Setting default RST_PIN to 4 (0 is not stable)

--- ev-esp32-rc522-0625/main/ev-esp32-rc522-0625.c
+++ ev-esp32-rc522-0625/main/ev-esp32-rc522-0625.c
@@ -5,6 +5,7 @@
 #include "Arduino.h"
 #include "ev_buzzer.h"
 #include "mqtt-client.h"
+#include "cJSON.h"
 
 #define LOOP_TASK_DELAY_MS 1000
 #define BEFORE_INIT_RFID_DELAY_MS 500
@@ -39,11 +40,55 @@ void bytes_to_hex_string(const byte *bytes, char *hex_str) {
     sprintf(hex_str, "%02X%02X%02X%02X", bytes[0], bytes[1], bytes[2], bytes[3]);
 }
 
+char* create_json_message(const char* cardId) {
+    cJSON *root = cJSON_CreateObject();
+    cJSON_AddStringToObject(root, "cardId", cardId);
+    char *json_string = cJSON_PrintUnformatted(root);
+    cJSON_Delete(root);
+    return json_string;  // Remember to free this after sending
+}
+
+bool parse_access_granted(const char* json_str) {
+    cJSON *root = cJSON_Parse(json_str);
+    if (root == NULL) {
+        ESP_LOGW(TAG, "cJSON_Parse failed");
+        return false;
+    }
+
+    cJSON *access_granted = cJSON_GetObjectItem(root, "accessGranted");
+    if (!cJSON_IsBool(access_granted)) {
+        ESP_LOGW(TAG, "Attribute 'accessGranted' is not a boolean");
+        cJSON_Delete(root);
+        return false;
+    }
+
+    bool result = cJSON_IsTrue(access_granted);
+    cJSON_Delete(root);
+    return result;
+}
+
+void mqtt_data_handler(const char *topic, const char *data) {
+    ESP_LOGI(TAG, "Message arrived on topic: %s", topic);
+
+    bool access_granted = parse_access_granted(data);
+    if (access_granted) {
+        ESP_LOGI(TAG, "Access granted");
+        play_access_granted();
+    } else {
+        ESP_LOGI(TAG, "Access denied");
+        play_access_denied();
+    }
+}
+
 void rfid_card_handler(const byte* cardId) {
+    // Format to hex
     char hex_str[4];
     bytes_to_hex_string(cardId, hex_str);
     ESP_LOGI(TAG, "Card ID: %s", hex_str);
-    play_access_granted();
+
+    // Send to MQTT
+    const char* data = create_json_message(hex_str);
+    mqtt_publish_telemetry(data);
 }
 
 void gpio_pins_setup(void) {
@@ -60,7 +105,7 @@ void app_main(void)
     init_wifi();
 
     // Setup MQTT client
-    mqtt_app_start();
+    mqtt_app_start(mqtt_data_handler);
 
     // Init buzzer
     buzzer_init();
